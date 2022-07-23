2c2d2d0d1f4103d95f39bd0dd77cf81d14dbed8b
akshayhl2196 <109200073+akshayhl2196@users.noreply.github.com>
2022-07-23 12:35:08 +0530

    Update sql-backup.sh

--- sql-backup.sh
+++ sql-backup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 
-USER="root"
+USER="_"
 # PASSWORD="PASSWORD"
-HOST="testdatbase.ckt1rvkddibh.us-east-1.rds.amazonaws.com"
-DB_NAME="testdatbase"
+HOST="Database Host"
+DB_NAME="_"
 
 
 #Backup_Directory_Locations
 
 BACKUPROOT="/backup/mysql_dump"
 TSTAMP=$(date +"%d-%b-%Y-%H-%M-%S")
-S3BUCKET="s3://backup-demo2022"
+S3BUCKET="s3://<Bucket_Name>
 
 
 
@@ -20,9 +20,9 @@ S3BUCKET="s3://backup-demo2022"
 LOG_ROOT="/backup/mysql_dump/logs/dump.log"
 #Dump of Mysql Database into S3\
 echo "$(tput setaf 2)creating backup of database start at $TSTAMP" >> "$LOG_ROOT"
-mysqldump  -h testdatbase.ckt1rvkddibh.us-east-1.rds.amazonaws.com  -u admin -p"VuUMkv9phvfMCQ5KyNPI" > $BACKUPROOT/$DB_NAME-$TSTAMP.sql | aws s3 cp - s3://backup-demo2022/test-database-dump.sql
+mysqldump  -h "<Database Host>  -u<USER NAME>  -p"PASSWORD" > $BACKUPROOT/$DB_NAME-$TSTAMP.sql | aws s3 cp - s3://<bucket name>/<file name.sql>
 
-~
+
 ~
 ~
 ~
